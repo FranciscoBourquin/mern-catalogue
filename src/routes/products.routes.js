8bba6a96484eacde5128a13ad58911013982c9fc
Francisco Bourquin <fran.bourquin89@gmail.com>
2025-08-01 17:36:29 -0300

    Se agrega controlador para actualizar productos

--- src/routes/products.routes.js
+++ src/routes/products.routes.js
@@ -1,13 +1,15 @@
 import { Router } from "express";
-import { createProductController, deleteProductByIdController, getProductByIdController, getProductsController } from "../controllers/products.controller.js";
+import { createProductController, deleteProductByIdController, getProductByIdController, getProductsController, updateProductByIdController } from "../controllers/products.controller.js";
 
 
 export const productsRouter = Router();
 
-productsRouter.get("/", getProductsController)
-
-productsRouter.get("/:id", getProductByIdController)
-
 productsRouter.post("/", createProductController);
 
-productsRouter.delete("/:id", deleteProductByIdController);
+productsRouter.get("/", getProductsController)
+
+productsRouter.get("product/:id", getProductByIdController)
+
+productsRouter.put("product/:id", updateProductByIdController)
+
+productsRouter.delete("product/:id", deleteProductByIdController);
