3aec99c87310b40334ce3a3c2ab5d11fdb66a8da
Francisco Bourquin <fran.bourquin89@gmail.com>
2025-09-13 13:42:38 -0300

    creación del directorio mocks y archivo products.js

---
+++ frontend/src/mocks/products.js
@@ -0,0 +1,75 @@
+export const products = [
+  {
+    category: "televisor",
+    name: 'Smart TV LG 32" HD LQ630BPSA',
+    price: "ARS 299.999,00",
+    image:
+      "https://http2.mlstatic.com/D_NQ_NP_749626-MLA91970917997_092025-O.webp",
+    source: "LG Shop Argentina",
+  },
+  {
+    category: "televisor",
+    name: 'Smart TV Philco 32" HD Vidaa PLD32HS24Vh',
+    price: "ARS 214.999,00",
+    image: "https://http2.mlstatic.com/D_NQ_NP_2X_XXXXX-MLAXXXXXX-F.webp",
+    source: "Frávega",
+  },
+  {
+    category: "televisor",
+    name: 'Smart TV Philco 43" Full HD PLD43FS25AH',
+    price: "ARS 399.999,00",
+    image: "https://http2.mlstatic.com/D_NQ_NP_2X_XXXXX-MLAXXXXXX-F.webp",
+    source: "On City",
+  },
+  {
+    category: "smartphone",
+    name: "Xiaomi Redmi Note 14",
+    price: "ARS 298.000,00",
+    image:
+      "https://encrypted-tbn1.gstatic.com/shopping?q=tbn:ANd9GcQQcIHbvC6Y6dAJog_x6uhLaWhZgjMjXUKbMzs2Zy8YFBA6qp0mIhX5T_rDBOmxn8jg-IdtHVlRQBWAuWkf51TeiS6Bbu8iXun5ILNTiBs_IumG5M5hMk_2MH-VqTBkMwyTpiCg1YAxqQ&usqp=CAc",
+    source: "MercadoLibre",
+  },
+  {
+    category: "smartphone",
+    name: "Realme Note 60",
+    price: "ARS 179.999,00",
+    image: "https://http2.mlstatic.com/D_NQ_NP_2X_XXXXXXX-MLAXXXXXX-F.webp",
+    source: "Frávega",
+  },
+  {
+    category: "smartphone",
+    name: "Samsung Galaxy A15",
+    price: "ARS 271.999,00",
+    image: "https://http2.mlstatic.com/D_NQ_NP_2X_XXXXXXX-MLAXXXXXX-F.webp",
+    source: "Novogar",
+  },
+  {
+    category: "laptop",
+    name: "Lenovo Loq Intel RTX",
+    price: "ARS 944.012,88",
+    image:
+      "https://encrypted-tbn0.gstatic.com/shopping?q=tbn:ANd9GcRjReDykmewilDo4xYmPVYn1llYRIefSeDm0FfpOvEVCAE-g2SxfgpVdDdNovICLet5zueRIHoVBcTr3Cr_pC2ktqLbm0nZdaHFuoY5R_Th&usqp=CAc",
+    source: "MercadoLibre",
+  },
+  {
+    category: "laptop",
+    name: "Lenovo Ideapad Slim 3",
+    price: "ARS 904.199,00",
+    image: "https://http2.mlstatic.com/D_NQ_NP_2X_XXXXXXX-MLAXXXXXX-F.webp",
+    source: "Cetrogar",
+  },
+  {
+    category: "laptop",
+    name: 'HP 14" 4GB / 128GB Windows 11 Home (14-dq0536la)',
+    price: "ARS 617.999,00",
+    image: "https://http2.mlstatic.com/D_NQ_NP_2X_XXXXXXX-MLAXXXXXX-F.webp",
+    source: "Frávega",
+  },
+  {
+    category: "televisor",
+    name: 'Smart TV LED Admiral HD 32" AD32E3E',
+    price: "ARS 254.999,00",
+    image: "https://http2.mlstatic.com/D_NQ_NP_2X_XXXXXXX-MLAXXXXXX-F.webp",
+    source: "Frávega",
+  },
+];
