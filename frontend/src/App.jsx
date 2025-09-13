f2718c3e57169ef24f6cd1f5b0e7d7221b0cf480
Francisco Bourquin <fran.bourquin89@gmail.com>
2025-09-13 16:39:32 -0300

    Agregado de rutas

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,9 +1,20 @@
-import { CategoriesCard } from "./components/CategoriesCard"
+import { Routes, Route } from "react-router-dom"
+import { Home } from "./pages/Home"
+import { Tv } from "./pages/Tv"
+import { Smartphones } from "./pages/Smartphones"
+import { Laptops } from "./pages/Laptops"
+import { Navbar } from "./components/Navbar"
 
 function App() {
   return (
     <>
-      <CategoriesCard />
+      <Navbar/>
+      <Routes>
+        <Route path="/" element= {<Home />}/>
+        <Route path="/productos/televisores" element= {<Tv/>}/>
+        <Route path="/productos/smartphones" element= {<Smartphones/>}/>
+        <Route path="/productos/laptops" element= {<Laptops/>}/>
+      </Routes>
     </>
   )
 }
